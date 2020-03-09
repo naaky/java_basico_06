c6da692b48f1edf7932ff2c8b36152c89b422b35
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:43:42 -0300

    exercicios a e b

--- src/java_basico_06/Main.java
+++ src/java_basico_06/Main.java
@@ -3,8 +3,25 @@ package java_basico_06;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		Main app = new Main();
+		app.start();
 
+	}
+
+	private void start() {
+		
+		String[] carros = { "civic", "gol", "palio", "uno"};
+		
+		System.out.println("Posicao 1: " + carros[1]);
+		
+		for (String carro : carros) {
+			
+			System.out.println("Carro: " + carro);
+		}
+		
+		System.out.println("");
+		
 	}
 
 }
